e69ded2be0d6eafb1ee764fbc8cac763ef5339a3
Eduardo Serrano <eduardoserrano8591@gmail.com>
2023-05-15 14:23:53 -0500

    Adds components to signupScreen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,21 +1,30 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import { View, StyleSheet } from "react-native";
+import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
   return (
     <>
-      <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-      <Button
-        title="go to signin"
-        onPress={() => navigation.navigate("Signin")}
-      />
-      <Button
-        title="Go to main flow"
-        onPress={() => navigation.navigate("mainFlow")}
-      />
+      <Spacer>
+        <Text h3> Sign up for Tracker</Text>
+      </Spacer>
+      <Spacer>
+        <Input label="Email" />
+      </Spacer>
+      <Spacer>
+        <Input label="Password" />
+      </Spacer>
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
     </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  spacer: {
+    margin: {},
+  },
+});
 export default SignupScreen;
